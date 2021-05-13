1d8548ca3026d29853171d64b08f97ee5a4e6aa2
Edgar Quiroz <equiroz@ciencias.unam.mx>
2021-05-13 15:39:29 -0500

    Correcting after run

--- scripts/cuda_c/simple_add.cu
+++ scripts/cuda_c/simple_add.cu
@@ -1,6 +1,6 @@
 #include <iostream>
 
-__global__ cu_add(int a, int b, int *c) {
+__global__ void cu_add(int a, int b, int *c) {
     *c = a + b;
 }
 
@@ -10,12 +10,12 @@ int main(void) {
 
     cudaMalloc((void**)&cuda_res, sizeof(int));
 
-    cu_add(10, 7, cuda_res);
+    cu_add<<<1, 1>>>(10, 7, cuda_res);
 
-    cudaMemcpy(&res, cuda_res, cudaMemxpyDeviceToHost);
+    cudaMemcpy(&res, cuda_res, sizeof(int), cudaMemcpyDeviceToHost);
 
     cudaFree(cuda_res);
 
-    printf("10 + 7 = %d, using CUDA!!!", res);
+    printf("10 + 7 = %d, using a CUDA kernel!!!\n", res);
     return 0;
 }
