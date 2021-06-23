33b77fd151f24d4a17e33e2d1e3bffc3c2b91fd4
Edgar Quiroz <eq24f02@pm.me>
2021-06-23 08:58:15 -0500

    Adding full cost function
    Tests don't pass

--- src/SupplyChains/src/SupplyChains.jl
+++ src/SupplyChains/src/SupplyChains.jl
@@ -1,10 +1,12 @@
 module SupplyChains
 
-export SupplyCapacity, SupplyCost, SupplySchedule,
-    dims, cost
+using LinearAlgebra
+
+export SupplyCapacity, SupplyCost, SupplyChain, SupplySchedule,
+    size, cost
 
 function check_eq(mess, e1, e2)
-    if (e1 != e2)
+    if e1 != e2
         error(mess * ": $e1 ≠ $e2")
     end
 end
@@ -16,28 +18,44 @@ struct SupplyCapacity
     clients
 end
 
-function dims(cap::SupplyCapacity)
+function Base.size(cap::SupplyCapacity)
     length.((cap.supliers, cap.plants, cap.distributors, cap.clients))
 end
 
 struct SupplyEdges
     supls_plants
-    plants_dist
+    plants_dists
     dists_clients
 
     function SupplyEdges(cost_sp, cost_pd, cost_dc)
         _, np = size(cost_sp)
         _np, nd = size(cost_pd)
-        check_eq("Incongruent matrices dims", np, _np)
+        check_eq("Incongruent matrices size", np, _np)
 
         _nd, _ = size(cost_dc)
-        check_eq("Incongruent matrices dims", nd, _nd)
+        check_eq("Incongruent matrices size", nd, _nd)
 
         new(cost_sp, cost_pd, cost_dc)
     end
 end
 
-function dims(e::SupplyEdges)
+function active_edges(e, active_plants, active_dists)
+    plants_matrix = Diagonal(active_plants)
+    dists_matrix = Diagonal(active_dists)
+    supl_plants_cost = e.supls_plants * plants_matrix
+    plants_dist_cost = plants_matrix * e.plants_dists * dists_matrix
+    dist_clients_cost = dists_matrix * e.dists_clients
+    SupplyEdges(supl_plants_cost, plants_dist_cost, dist_clients_cost)
+end
+
+function Base.vec(e::SupplyEdges)
+    sp = vec(e.supls_plants)
+    pd = vec(e.plants_dists)
+    dc = vec(e.dists_clients)
+    vcat(sp, pd, dc)
+end
+
+function Base.size(e::SupplyEdges)
     (size(e.supls_plants)..., size(e.dists_clients)...)
 end
 
@@ -48,9 +66,9 @@ struct SupplyCost
 
     function SupplyCost(fixed_p, fixed_d, cost_sp, cost_pd, cost_dc)
         unitary_cost = SupplyEdges(cost_sp, cost_pd, cost_dc)
-        d = dims(unitary_cost)
+        d = size(unitary_cost)
         check_eq(
-            "Incongruent fixed-unitary cost dims",
+            "Incongruent fixed-unitary cost size",
             (length(fixed_p), length(fixed_d)),
             (d[2], d[3])
         )
@@ -59,28 +77,56 @@ struct SupplyCost
     end
 end
 
-dims(cost::SupplyCost) = dims(cost.unitary)
+Base.size(cost::SupplyCost) = size(cost.unitary)
 
-struct SupplySchedule
+struct SupplyChain
     capacities
     costs
+
+    function SupplyChain(cap, cost)
+        check_eq("Incongruent cost-capacity matrices", size(cap), size(cost))
+        new(cap, cost)
+    end
+end
+
+Base.size(chain::SupplyChain) = size(chain.capacities)
+
+struct SupplySchedule
+    chain
     max_plants
     max_distributors
     active_plants
     active_distributors
     load
 
-    function SupplySchedule(cap, cost, mp, md, ap, ad, flowsp, flowpd, flowdc)
-        check_eq("Incongruent cost-capacity matrices", dims(cap), dims(cost))
-        flow = SupplyEdges(flowsp, flowpd, flowdc)
-        new(cap, cost, mp, md, ap, ad, flow)
+    function SupplySchedule(chain, mp, md, ap, ad, load)
+        dims = size(chain)
+        check_eq("Incongruent flow-chain dims", dims, size(load))
+        check_eq("Incongruent number of plants", length(ap), dims[2])
+        check_eq("Incongruent number of distributors", length(ap), dims[3])
+        new(chain, mp, md, ap, ad, load)
     end
 end
 
-function cost(s::SupplySchedule)
-    total_active = transpose(vcat(s.active_plants, s.active_distributors))
-    fixed_cost = vcat(s.costs.plants, s.costs.distributors)
-    total_active * fixed_cost
+function SupplySchedule(cap, cost, mp, md, ap, ad, flowsp, flowpd, flowdc)
+    chain = SupplyChain(cap, cost)
+    flow = SupplyEdges(flowsp, flowpd, flowdc)
+    SupplySchedule(chain, mp, md, ap, ad, flow)
+end
+
+function cost(s)
+    active_unit_cost = vec(active_edges(
+        s.chain.costs.unitary,
+        s.active_plants,
+        s.active_distributors
+    ))
+    active_load_cost = transpose(active_unit_cost) * vec(s.load)
+    fixed_cost = vcat(s.chain.costs.plants, s.chain.costs.distributors)
+    println(s.active_plants)
+    active_fixed_cost = transpose(fixed_cost) * vcat(
+        s.active_plants, s.active_distributors
+    )
+    active_load_cost + active_fixed_cost
 end
 
 end # module
--- src/SupplyChains/test/runtests.jl
+++ src/SupplyChains/test/runtests.jl
@@ -1,7 +1,7 @@
 using Test, SupplyChains
 
 @testset "SupplyChains" begin
-    z = hcat(1)
+    z = hcat(0)
     empty_cap = SupplyCapacity([0], [0], [0], [0])
     empty_cost = SupplyCost([0], [0], z, z, z)
     empty_sch = SupplySchedule(empty_cap, empty_cost, 0, 0, [0], [0], z, z, z)
@@ -14,6 +14,16 @@ using Test, SupplyChains
     end
 
     @testset "Cost" begin
-    @test cost(empty_sch) == 0
+        o = [1 1; 1 1]
+        z2 = [0, 0]
+        r = rand(1:10, (2, 2))
+        rand_unit_cost_schedule = SupplySchedule(
+            SupplyCapacity(z2, z2, z2, z2),
+            SupplyCost(z2, z2, r, r, r),
+            0, 0, [1, 1], [1, 1], o, o, o
+        )
+        dims = size(rand_unit_cost_schedule.chain)
+        n_edges = dims[1]*dims[2]+dims[2]*dims[3]+dims[3]*dims[4]
+        @test cost(rand_unit_cost_schedule) == 12*sum(r)
     end
 end
